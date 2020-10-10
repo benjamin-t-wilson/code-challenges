9fb166307c6e2915daabbb95f342be196455239b
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-10-10 11:27:14 -0700

    add C# files

---
+++ C# Solution/Code-Challenges/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Code_Challenges
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var EightKyu = new EightKyu();
+            Console.WriteLine(EightKyu.InvertValues(new int[] { 1, 2, 3, 4, 5 }));
+            Console.ReadLine();
+        }
+    }
+}
